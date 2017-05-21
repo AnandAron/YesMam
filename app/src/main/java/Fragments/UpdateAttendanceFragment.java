e7e17bccd2c51ab69ada57ebbffbb5e41aeed341
Anand <anandaron@gmail.com>
2017-05-21 14:57:31 +0530

    Button Toggle, Attendance Mapping and submit button

--- app/src/main/java/Fragments/UpdateAttendanceFragment.java
+++ app/src/main/java/Fragments/UpdateAttendanceFragment.java
@@ -1,8 +1,10 @@
 package Fragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,12 +20,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UpdateAttendanceFragment extends android.app.Fragment {
     private  static  int n;
-
+    final Map<String,String> attendance = new HashMap<String,String>();
 
     public UpdateAttendanceFragment() {
         // Required empty public constructor
@@ -37,6 +45,8 @@ public class UpdateAttendanceFragment extends android.app.Fragment {
         final View rootView =inflater.inflate(R.layout.fragment_attendance, container, false);
         final ProgressBar progressBar = (ProgressBar) rootView.findViewById(R.id.attendanceProgressBar) ;
         progressBar.setVisibility(View.VISIBLE);
+        final Button submit = (Button) rootView.findViewById(R.id.att_submit);
+        submit.setVisibility(View.INVISIBLE);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("CSE5").child("PersDet");
         myRef.addValueEventListener(new ValueEventListener() {
@@ -44,14 +54,45 @@ public class UpdateAttendanceFragment extends android.app.Fragment {
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 n = (int)dataSnapshot.getChildrenCount();
+
+                final Iterator<DataSnapshot> children = dataSnapshot.getChildren().iterator();
                 GridLayout layout = (GridLayout) rootView.findViewById(R.id.attendance_grid);
                 int i;
 
-                for(i=1;i<=n;i++){
-                    Button btn = new Button(getActivity());
-                    btn.setText(""+(15500+i));
+                while(children.hasNext()){
+                    final Button btn = new Button(getActivity());
+                    btn.setBackgroundColor(Color.WHITE);
+                    final String key = children.next().getKey().toString();
+                    btn.setText(key);
+                    btn.setTag(key);
+
+                    attendance.put(key,"A");
                     layout.addView(btn);
+                    btn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if (attendance.get(key).equals("P")){
+                                attendance.put(key,"A");
+
+                                btn.setBackgroundColor(Color.WHITE);
+                            }
+
+                            else if (attendance.get(key).equals("A")){
+                                attendance.put(key,"P");
+                                btn.setBackgroundColor(Color.BLUE);
+                            }
+
+                        }
+                    });
                 }
+                submit.setVisibility(View.VISIBLE);
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Toast.makeText(getActivity(),attendance.values().toString(),Toast.LENGTH_SHORT).show();
+                    }
+                });
                 progressBar.setVisibility(View.INVISIBLE);
                 Toast.makeText(getActivity(),""+dataSnapshot.getChildrenCount(),Toast.LENGTH_SHORT).show();
             }
